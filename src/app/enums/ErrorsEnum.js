237e730a7cba35945619dfe8a886f2ca450d4bb5
Iván Alfredo López Barrera <ivan23.barrera@gmail.com>
2025-08-19 00:49:03 -0600

    Feat: Adding new errors and warnings

--- src/app/enums/ErrorsEnum.js
+++ src/app/enums/ErrorsEnum.js
@@ -14,6 +14,12 @@ export const ErrorsEnum = {
     STR: field => `The \`${field}\` element must contain a valid string.`,
     STRNUMBER: field => `The \`${field}\` element must contain a valid ` +
                         'string or a valid number.',
+    MINLEN: (field, value) => {
+        const plural = (value > 1) ? 's' : '';
+
+        return `The \`${field}\` element must contain at least ${value} ` +
+               `character${plural}.`;
+    },
     IN: (field, comparison) => inError(field, comparison),
     INLOWER: (field, comparison) => inError(
         field,
--- src/app/enums/WarningsEnum.js
+++ src/app/enums/WarningsEnum.js
@@ -1,5 +1,21 @@
 export const WarningsEnum = {
     USESTRING: value => 
         'To maintain precision, always try to send `from.value` in string ' +
-        `format, not number format: '${value}' instead of ${value}.`
+        `format, not number format: '${value}' instead of ${value}.`,
+    TOOMANYDECIMALS: () =>
+        'The result had too many decimals. These were truncated to only ' +
+        'twenty characters.',
+
+    CHEXADECIMALVAL: value =>
+        `\`${value}\` is not a valid hexadecimal number.`,
+    CDECIMALVAL: value =>
+        `\`${value}\` is not a valid decimal number.`,
+    COCTALVAL: value =>
+        `\`${value}\` is not a valid octal number.`,
+    CBINARYVAL: value =>
+        `\`${value}\` is not a valid binary number.`,
+    CBASE62VAL: value =>
+        `\`${value}\` is not a valid number in base 62 format.`,
+    CTEXTVAL: value =>
+        `\`${value}\` is not a valid number in text format.`,
 }
